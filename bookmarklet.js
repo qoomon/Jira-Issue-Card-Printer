89a034ba300449c10387970b79f8140f8697ef52
Bengt Brodersen <qoomon@users.noreply.github.com>
2015-01-27 23:06:09 +0100

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,7 +4,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-50840116-3', 'auto', {'name': 'jiraIssueCardPrinter'});
+ga('create', 'UA-50840116-3', 'auto');
 ga('send', 'pageview');
 // </GoogleAnalytics>
 
