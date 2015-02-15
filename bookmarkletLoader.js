6229f85d382affa520df7733cdec1dab7a8d17da
Bengt Brodersen <qoomon@users.noreply.github.com>
2015-02-15 19:19:47 +0100

    Update bookmarkletLoader.js

--- bookmarkletLoader.js
+++ bookmarkletLoader.js
@@ -2,7 +2,7 @@
 javascript:(function(){
         var head = document.getElementsByTagName("head")[0];
         var scriptElement = document.createElement("script");
-        scriptElement.src = <URL to Script>;	
+        scriptElement.src = 'https://qoomon.github.io/Jira-Issue-Card-Printer/bookmarklet.js';	
         head.appendChild(scriptElement);
         head.removeChild(scriptElement);
 })();
