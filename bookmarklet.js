0b0afdcae993f72bfd1228ea18a1ec534ebdb31f
Bengt Brodersen <b.brodersen@xplosion.de>
2015-11-27 15:02:05 +0100

    refactored date format function

--- bookmarklet.js
+++ bookmarklet.js
@@ -6,7 +6,7 @@
   // YouTrack: http://qoomon.myjetbrains.com/youtrack/dashboard
 
   var global = {};
-  global.version = "4.2.8";
+  global.version = "4.2.9";
   global.issueTrackingUrl = "https://github.com/qoomon/Jira-Issue-Card-Printer";
   global.isDev = document.currentScript == null;
   global.isProd = !global.isDev;
@@ -117,7 +117,6 @@
 
     console.log("Init...")
     addStringFunctions();
-    addDateFunctions();
 
     loadSettings();
 
@@ -164,16 +163,14 @@
 
   function loadSettings(){
     var settings = global.settings = global.settings || {};
-    settings.scale = readCookie("card_printer_font_scale", 1);
-    settings.rowCount = readCookie("card_printer_row_count2", 2);
-    settings.colCount = readCookie("card_printer_column_count", 1);
+    settings.scale = parseFloat(readCookie("card_printer_font_scale")) || 1.0;
+    settings.rowCount = parseInt(readCookie("card_printer_row_count2")) || 2;
+    settings.colCount = parseInt(readCookie("card_printer_column_count")) || 1;
 
-    settings.singleCardPage = (readCookie("card_printer_single_card_page", 'true') == 'true');
-    settings.hideDescription = (readCookie("card_printer_hide_description", 'false') == 'true');
-    settings.hideAssignee = (readCookie("card_printer_hide_assignee", 'true') == 'true');
-    settings.hideDueDate = (readCookie("card_printer_hide_due_date", 'false') == 'true');
-
-    console.log("settings: "+JSON.stringify(settings,2,2))
+    settings.singleCardPage = parseBool(readCookie("card_printer_single_card_page"), true );
+    settings.hideDescription = parseBool(readCookie("card_printer_hide_description"), false);
+    settings.hideAssignee = parseBool(readCookie("card_printer_hide_assignee"), false);
+    settings.hideDueDate = parseBool(readCookie("card_printer_hide_due_date"), false);
   }
 
   function print() {
@@ -584,20 +581,6 @@
   //############################################################################################################################
   //############################################################################################################################
 
-  function appendScript(url, callback) {
-
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.src = url;
-
-    // Then bind the event to the callback function.
-    // There are several events for cross browser compatibility.
-    script.onreadystatechange = callback;
-    script.onload = callback;
-
-    head.appendChild(script);
-  }
-
   function initGoogleAnalytics() {
     // <GoogleAnalytics>
     (function(i, s, o, g, r, a, m) {
@@ -622,18 +605,61 @@
   //############################################################################################################################
   //############################################################################################################################
 
-  function readCookie(name, defaultValue) {
+  function parseBool(text, def){
+    if(text == 'true') return true;
+    else if ( text == 'false') return false;
+    else return def;
+  }
+
+  function appendScript(url, callback) {
+
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    head.appendChild(script);
+  }
+
+  function readCookie(name) {
     var cookies = document.cookie.split('; ');
 
     for (var i = 0; i < cookies.length; i++) {
       var cookie = cookies[i].split('=');
       if (cookie[0] == name) return cookie[1];
     }
-    return defaultValue
+    return null;
   }
 
   function writeCookie(name, value) {
     document.cookie = name + "=" + value;
+  }
+
+  function httpGetCORS(){
+    arguments[0] = 'https://jsonp.afeld.me/?url=' + arguments[0];
+    return httpGet.apply(this, arguments);
+  }
+
+  function httpGet(){
+    return Promise.resolve(jQuery.get.apply(this, arguments));
+  }
+
+  function httpGetJSON(){
+    return Promise.resolve(jQuery.getJSON.apply(this, arguments));
+  }
+
+  function multilineString(commentFunction) {
+    return commentFunction.toString()
+      .replace(/^[^\/]+\/\*!?/, '')
+      .replace(/\*\/[^\/]+$/, '');
+  }
+
+  function resizeIframe(iframe) {
+    iframe.height(iframe[0].contentWindow.document.body.height);
   }
   //############################################################################################################################
   //############################################################################################################################
@@ -678,193 +704,11 @@
     }
   }
 
-  function addDateFunctions() {
-
-    Date.prototype.format = function(format) {
-      var returnStr = '';
-      var replace = Date.replaceChars;
-      for (var i = 0; i < format.length; i++) {
-        var curChar = format.charAt(i);
-        if (i - 1 >= 0 && format.charAt(i - 1) == "\\") {
-          returnStr += curChar;
-        } else if (replace[curChar]) {
-          returnStr += replace[curChar].call(this);
-        } else if (curChar != "\\") {
-          returnStr += curChar;
-        }
-      }
-      return returnStr;
-    };
-
-    Date.replaceChars = {
-      shortMonths: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-      longMonths: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      shortDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-      longDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-
-      // Day
-      d: function() {
-        return (this.getDate() < 10 ? '0' : '') + this.getDate();
-      },
-      D: function() {
-        return Date.replaceChars.shortDays[this.getDay()];
-      },
-      j: function() {
-        return this.getDate();
-      },
-      l: function() {
-        return Date.replaceChars.longDays[this.getDay()];
-      },
-      N: function() {
-        return this.getDay() + 1;
-      },
-      S: function() {
-        return (this.getDate() % 10 == 1 && this.getDate() != 11 ? 'st' : (this.getDate() % 10 == 2 && this.getDate() != 12 ? 'nd' : (this.getDate() % 10 == 3 && this.getDate() != 13 ? 'rd' : 'th')));
-      },
-      w: function() {
-        return this.getDay();
-      },
-      z: function() {
-        var d = new Date(this.getFullYear(), 0, 1);
-        return Math.ceil((this - d) / 86400000);
-      }, // Fixed now
-      // Week
-      W: function() {
-        var d = new Date(this.getFullYear(), 0, 1);
-        return Math.ceil((((this - d) / 86400000) + d.getDay() + 1) / 7);
-      }, // Fixed now
-      // Month
-      F: function() {
-        return Date.replaceChars.longMonths[this.getMonth()];
-      },
-      m: function() {
-        return (this.getMonth() < 9 ? '0' : '') + (this.getMonth() + 1);
-      },
-      M: function() {
-        return Date.replaceChars.shortMonths[this.getMonth()];
-      },
-      n: function() {
-        return this.getMonth() + 1;
-      },
-      t: function() {
-        var d = new Date();
-        return new Date(d.getFullYear(), d.getMonth(), 0).getDate()
-      }, // Fixed now, gets #days of date
-      // Year
-      L: function() {
-        var year = this.getFullYear();
-        return (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0));
-      }, // Fixed now
-      o: function() {
-        var d = new Date(this.valueOf());
-        d.setDate(d.getDate() - ((this.getDay() + 6) % 7) + 3);
-        return d.getFullYear();
-      }, //Fixed now
-      Y: function() {
-        return this.getFullYear();
-      },
-      y: function() {
-        return ('' + this.getFullYear()).substr(2);
-      },
-      // Time
-      a: function() {
-        return this.getHours() < 12 ? 'am' : 'pm';
-      },
-      A: function() {
-        return this.getHours() < 12 ? 'AM' : 'PM';
-      },
-      B: function() {
-        return Math.floor((((this.getUTCHours() + 1) % 24) + this.getUTreminutes() / 60 + this.getUTCSeconds() / 3600) * 1000 / 24);
-      }, // Fixed now
-      g: function() {
-        return this.getHours() % 12 || 12;
-      },
-      G: function() {
-        return this.getHours();
-      },
-      h: function() {
-        return ((this.getHours() % 12 || 12) < 10 ? '0' : '') + (this.getHours() % 12 || 12);
-      },
-      H: function() {
-        return (this.getHours() < 10 ? '0' : '') + this.getHours();
-      },
-      i: function() {
-        return (this.getMinutes() < 10 ? '0' : '') + this.getMinutes();
-      },
-      s: function() {
-        return (this.getSeconds() < 10 ? '0' : '') + this.getSeconds();
-      },
-      u: function() {
-        var m = this.getMilliseconds();
-        return (m < 10 ? '00' : (m < 100 ? '0' : '')) + m;
-      },
-      // Timezone
-      e: function() {
-        return "Not Yet Supported";
-      },
-      I: function() {
-        var DST = null;
-        for (var i = 0; i < 12; ++i) {
-          var d = new Date(this.getFullYear(), i, 1);
-          var offset = d.getTimezoneOffset();
-          if (DST === null) DST = offset;
-          else if (offset < DST) {
-            DST = offset;
-            break;
-          } else if (offset > DST) break;
-        }
-        return (this.getTimezoneOffset() == DST) | 0;
-      },
-      O: function() {
-        return (-this.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(this.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(this.getTimezoneOffset() / 60)) + '00';
-      },
-      P: function() {
-        return (-this.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(this.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(this.getTimezoneOffset() / 60)) + ':00';
-      }, // Fixed now
-      T: function() {
-        var m = this.getMonth();
-        this.setMonth(0);
-        var result = this.toTimeString().replace(/^.+ \(?([^\)]+)\)?$/, '$1');
-        this.setMonth(m);
-        return result;
-      },
-      Z: function() {
-        return -this.getTimezoneOffset() * 60;
-      },
-      // Full Date/Time
-      c: function() {
-        return this.format("Y-m-d\\TH:i:sP");
-      }, // Fixed now
-      r: function() {
-        return this.toString();
-      },
-      U: function() {
-        return this.getTimep() / 1000;
-      }
-    };
-  }
-
-  function httpGetCORS(){
-    arguments[0] = 'https://jsonp.afeld.me/?url=' + arguments[0];
-    return httpGet.apply(this, arguments);
-  }
-
-  function httpGet(){
-    return Promise.resolve(jQuery.get.apply(this, arguments));
-  }
-
-  function httpGetJSON(){
-    return Promise.resolve(jQuery.getJSON.apply(this, arguments));
-  }
-
-  function multilineString(commentFunction) {
-    return commentFunction.toString()
-      .replace(/^[^\/]+\/\*!?/, '')
-      .replace(/\*\/[^\/]+$/, '');
-  }
-
-  function resizeIframe(iframe) {
-    iframe.height(iframe[0].contentWindow.document.body.height);
+  function formatDate(date) {
+    var shortMonths = {'Jan': 1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12 };
+    var dateSplit = date.toString().split(" ");
+    // Mo 28.11.
+    return dateSplit[0] + " " + dateSplit[2] + "." + shortMonths[dateSplit[1]] + ".";
   }
 
   // APP Specific Functions
@@ -941,7 +785,7 @@
         }
 
         if (data.fields.duedate) {
-          issueData.dueDate = new Date(data.fields.duedate).format('D d.m.');
+          issueData.dueDate = formatDate(new Date(data.fields.duedate));
         }
 
         issueData.hasAttachment = data.fields.attachment.length > 0;
@@ -967,7 +811,7 @@
         if (true) {
           //Desired-Date
           if (data.fields.desiredDate) {
-            issueData.dueDate = new Date(data.fields.desiredDate).format('D d.m.');
+            issueData.dueDate = formatDate(new Date(data.fields.desiredDate));
           }
         }
 
@@ -1097,7 +941,7 @@
         }
 
         if (data.deadline) {
-          issueData.dueDate = new Date(data.deadline).format('D d.m.');
+          issueData.dueDate = formatDate(new Date(data.deadline));
         }
 
         // TODO
@@ -1151,7 +995,7 @@
         }
 
         if (data.due) {
-          issueData.dueDate = new Date(data.due).format('D d.m.');
+          issueData.dueDate = formatDate(new Date(data.due));
         }
 
         issueData.hasAttachment = data.attachments > 0;
