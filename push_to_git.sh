b4752c971a202e05c8a3e506fdbc7c4b820760e7
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-12-15 14:17:41 +0100

    no message

--- push_to_git.sh
+++ push_to_git.sh
@@ -8,22 +8,23 @@ TARGET_BRANCH="$3"
 
 CLONE_FOLDER='.deploy'
 
-echo "Deploy to '${TARGET_BRANCH}'"
+echo "Deploy '$SOURCE_FOLDER' to '${TARGET_BRANCH}' '$TARGET_BRANCH'"
+rm -rf .deploy "$CLONE_FOLDER"
 
-echo "Clone '${TARGET_BRANCH}' to '${CLONE_FOLDER}'"
+echo "  Clone '${TARGET_BRANCH}' to '${CLONE_FOLDER}'"
 git clone --branch ${TARGET_BRANCH} --depth 1  ${TARGET_REPO} ${CLONE_FOLDER}
 cd ${CLONE_FOLDER}; git rm -r . --ignore-unmatch --quiet
 
-echo "Apply '${SOURCE_FOLDER}'"
+echo "  Apply '${SOURCE_FOLDER}'"
 cp -R ../${SOURCE_FOLDER}/* ./
 git add .
 
-echo "Commit changes to '${TARGET_BRANCH}'"
+echo "  Commit changes to '${TARGET_BRANCH}'"
 git config user.name 'Travis'
 git config user.email '<>'
 git commit -m "Travis Build" -m "Source Branch ${SOURCE_BRANCH}" -m "$(git show -s --format='short')"
 
-echo "Push to '${TARGET_BRANCH}'"
+echo "  Push to '${TARGET_BRANCH}'"
 git push -q origin ${TARGET_BRANCH}
 
 
