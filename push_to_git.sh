f42ff933ffe4628924185db5acbf0a4727b909f5
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-12-15 15:34:31 +0100

    chore: beautify push to git script

--- push_to_git.sh
+++ push_to_git.sh
@@ -8,23 +8,27 @@ TARGET_BRANCH="$3"
 
 CLONE_FOLDER='.deploy'
 
-echo "Deploy '$SOURCE_FOLDER' to '${TARGET_BRANCH}' '$TARGET_BRANCH'"
+echo "Deploy '$SOURCE_FOLDER' to '${TARGET_REPO}' '$TARGET_BRANCH'"
+
+echo "--- Clone Repo"
 rm -rf .deploy "$CLONE_FOLDER"
+git clone --branch "${TARGET_BRANCH}" --depth 1 "${TARGET_REPO}" "${CLONE_FOLDER}"
+(
+    cd "${CLONE_FOLDER}";
 
-echo "--- Clone '${TARGET_BRANCH}' to '${CLONE_FOLDER}'"
-git clone --branch ${TARGET_BRANCH} --depth 1  ${TARGET_REPO} ${CLONE_FOLDER}
-cd ${CLONE_FOLDER}; git rm -r . --ignore-unmatch --quiet
+    echo "--- Apply Changes"
+    git rm -r . --ignore-unmatch --quiet
+    cp -R ../${SOURCE_FOLDER}/* ./
+    git add .
 
-echo "--- Apply '${SOURCE_FOLDER}'"
-cp -R ../${SOURCE_FOLDER}/* ./
-git add .
+    echo "--- Commit Changes"
+    git config user.name 'Travis'
+    git config user.email '<>'
+    git commit -m "Travis Build" -m "Source Branch ${SOURCE_BRANCH}" -m "$(git show -s --format='short')"
 
-echo "--- Commit changes to '${TARGET_BRANCH}'"
-git config user.name 'Travis'
-git config user.email '<>'
-git commit -m "Travis Build" -m "Source Branch ${SOURCE_BRANCH}" -m "$(git show -s --format='short')"
-
-echo "--- Push to '${TARGET_BRANCH}'"
-git push -q origin ${TARGET_BRANCH}
+    echo "--- Push Changes"
+    git push -q 'origin' "${TARGET_BRANCH}"
+)
+rm -rf .deploy "$CLONE_FOLDER"
 
 
