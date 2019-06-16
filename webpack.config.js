c4edec7ed1982bf86d6bb79fb53474685dcf5d55
Bengt Brodersen <bengt.brodersen@gmail.com>
2019-06-16 21:19:06 +0200

    build: update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const path = require('path')
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
